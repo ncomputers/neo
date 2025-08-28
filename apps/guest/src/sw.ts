575c1d0d3ca5fa6eccc43f26eac396de241b5163
ncomputers <ncomputerudr@gmail.com>
2025-08-28 16:39:49 +0530

    chore(pwa): remove local icon placeholders

--- apps/guest/src/sw.ts
+++ apps/guest/src/sw.ts
@@ -1,7 +1,11 @@
-import { precacheAndRoute } from 'workbox-precaching';
+import { precacheAndRoute, createHandlerBoundToURL } from 'workbox-precaching';
+import { registerRoute, NavigationRoute } from 'workbox-routing';
 
 // @ts-ignore self is defined in service worker
 precacheAndRoute(self.__WB_MANIFEST);
+
+const navigationRoute = new NavigationRoute(createHandlerBoundToURL('/index.html'));
+registerRoute(navigationRoute);
 
 self.addEventListener('message', (event: any) => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
