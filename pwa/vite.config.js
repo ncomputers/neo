1f3b434ac1ad8ba3ad5495b77e6b6ec84dbc1703
ncomputers <ncomputerudr@gmail.com>
2025-08-28 15:36:37 +0530

    feat: centralize shared configs

--- pwa/vite.config.js
+++ pwa/vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import baseConfig from '@neo/config/vite'
 
 export default defineConfig({
+  ...baseConfig,
   plugins: [react()],
 })
