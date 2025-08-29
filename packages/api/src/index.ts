8f4409afd165d374689327e2e72b77f6217b079a
ncomputers <ncomputerudr@gmail.com>
2025-08-29 11:09:41 +0530

    feat(api): add menu management endpoints

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -2,4 +2,21 @@ export * from './api';
 export * from './hooks/sse';
 export * from './hooks/ws';
 export { usePageview } from './hooks/usePageview';
-export * from './endpoints';
+export {
+  loginPin,
+  getMenu,
+  placeOrder,
+  orderStream,
+  kdsTickets,
+  adminBilling,
+  getCategories,
+  createCategory,
+  deleteCategory,
+  getItems,
+  createItem,
+  updateItem,
+  deleteItem,
+  uploadImage,
+  exportMenuI18n,
+  importMenuI18n
+} from './endpoints';
